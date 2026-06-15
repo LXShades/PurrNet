21f484795def54d13be6dcc2581daf25e94a7007
LXShades <louis_foy@yahoo.co.uk>
2026-06-15 15:56:45 +0100

    Fixing error spam when connecting with UDPTransport with a malformed address or ip:port

--- Assets/PurrNet/Runtime/Transports/UDPTransport.cs
+++ Assets/PurrNet/Runtime/Transports/UDPTransport.cs
@@ -248,7 +248,7 @@ namespace PurrNet.Transports
                 _ => DisconnectReason.ClientRequest
             };
 
-            onDisconnected?.Invoke(new Connection(peer.Id), reason, false);
+            onDisconnected?.Invoke(peer != null ? new Connection(peer.Id) : new Connection(0, false), reason, false);
         }
 
         private void OnClientConnected(NetPeer peer)
